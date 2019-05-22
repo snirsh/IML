55013a972685a1052494b3d84e06d88595643a4c
Snir <s.sharristh@gmail.com>
2019-05-22 20:25:46 +0300

    adaboost and perceptron are done

--- ex4/adaboost.py
+++ ex4/adaboost.py
@@ -54,7 +54,7 @@ class AdaBoost(object):
         Predict only with max_t weak learners,
         """
         h_predict = [self.h[t].predict(X) for t in range(max_t)]
-        return np.sign([np.dot([h_predict[t], self.w[t]) for t in range(max_t)])
+        return np.sign([np.dot(h_predict[t], self.w[t]) for t in range(max_t)])
 
     def error(self, X, y, max_t):
         """
