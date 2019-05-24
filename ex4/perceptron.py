ffd0012efd11084919f55e3c66a0bd2e5cede703
Snir <s.sharristh@gmail.com>
2019-05-24 11:24:08 +0300

    adaboost and perceptron are done

--- ex4/perceptron.py
+++ ex4/perceptron.py
@@ -5,17 +5,15 @@ class perceptron:
 
     def __init__(self):
         self.w = None
-        self.b = None
 
     def fit(self, X, y):
         self.w = np.zeros((X.shape[1], ))
         while True:
             w_t = self.w
             ywx = y*np.dot(X, self.w)
-            # ywx = y*wx
             neg_prod = np.argwhere(ywx <= 0)
             if len(neg_prod) != 0:
-                self.w = self.w + y[neg_prod[0][0]] + X[neg_prod[0][0]]
+                self.w = self.w + y[neg_prod[0][0]] * X[neg_prod[0][0]]
             else:
                 return w_t
 
